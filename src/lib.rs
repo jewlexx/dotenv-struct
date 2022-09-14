373c27bc0839ccff23abb80c54f5912a938b05dd
Juliette Cordor <grassojames5@gmail.com>
2022-09-14 14:05:49 +1000

    load .env path

--- src/lib.rs
+++ src/lib.rs
@@ -2,5 +2,10 @@ use quote::quote;
 
 #[proc_macro]
 pub fn dotenv(_item: proc_macro::TokenStream) -> proc_macro::TokenStream {
+    let dotenv_path = match dotenv::dotenv() {
+        Ok(v) => v,
+        Err(_) => return quote! { compile_error!("Could not find .env file") }.into(),
+    };
+
     quote! {}.into()
 }
