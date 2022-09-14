33825ce82a468efce9d76ad9dceeb92b52837ae4
Juliette Cordor <grassojames5@gmail.com>
2022-09-14 14:10:20 +1000

    fix

--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,29 @@
+use proc_macro2::TokenStream;
 use quote::quote;
 use std::fs::File;
 
+use thiserror::Error as AsError;
+
+#[derive(Debug, AsError)]
+enum DotenvError {
+    #[error("failed to read .env file")]
+    IoError(#[from] std::io::Error),
+    #[error("failed to parse .env file")]
+    ParseError(#[from] dotenv::Error),
+}
+
 #[proc_macro]
-pub fn dotenv(_item: proc_macro::TokenStream) -> proc_macro::TokenStream {
+pub fn dotenv(item: proc_macro::TokenStream) -> proc_macro::TokenStream {
+    dotenv_inner(item.into()).into()
+}
+
+fn dotenv_inner(_item: TokenStream) -> Result<TokenStream, DotenvError> {
     let dotenv_path = match dotenv::dotenv() {
         Ok(v) => v,
-        Err(_) => return quote! { compile_error!("Could not find .env file") }.into(),
+        Err(_) => return quote! { compile_error!("Could not find .env file") },
     };
 
     let file = File::open(dotenv_path);
 
-    quote! {}.into()
+    quote! {}
 }
